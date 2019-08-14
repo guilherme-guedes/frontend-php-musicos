dfc9a73a9500cc2cb7e151bd3dfd6801757d9e20
Guilherme Guedes <guilhermelgs@hotmail.com>
2019-08-14 20:03:14 -0300

    H's e tabela de resultado

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 
 <head>
     <link rel="stylesheet" type="text/css" href="css/reset.css">
-    <link rel="stylesheet" type="text/css" href="css/site.css">
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css?family=Bitter&display=swap" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/site.css">
 </head>
 
 <body>
